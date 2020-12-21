cf5de96fe446f31729bfe4c6d84ffbfcb30fe493
daelong <46810844+daelong@users.noreply.github.com>
2020-12-21 16:46:06 +0900

    아이템리스트 작성완료

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -17,6 +17,7 @@ const ItemBox = styled.div`
   margin-bottom: 1rem;
   &:hover {
     background-color: #ff7675;
+    cursor: pointer;
   }
 `;
 
@@ -26,7 +27,34 @@ const QuestionItem = () => {
   const [isHide2, setIsHide2] = useState(true);
   const [isHide3, setIsHide3] = useState(true);
   const [isHide4, setIsHide4] = useState(true);
-
+  const itemList = {
+    itemList: [
+      { name: "자장면", point: 0 },
+      { name: "짬뽕", point: 0 },
+      { name: "볶음밥", point: 0 },
+      { name: "마라탕", point: 0 },
+      { name: "초밥", point: 0 },
+      { name: "사케동", point: 0 },
+      { name: "라멘", point: 0 },
+      { name: "돈부리", point: 0 },
+      { name: "파스타", point: 0 },
+      { name: "피자", point: 0 },
+      { name: "리조또", point: 0 },
+      { name: "스테이크", point: 0 },
+      { name: "치킨", point: 0 },
+      { name: "회", point: 0 },
+      { name: "곱창", point: 0 },
+      { name: "전", point: 0 },
+      { name: "돈까스", point: 0 },
+      { name: "불닭볶음면", point: 0 },
+      { name: "라면", point: 0 },
+      { name: "삼겹살", point: 0 },
+      { name: "떡볶이", point: 0 },
+      { name: "국밥", point: 0 },
+    ],
+  };
+  itemList.itemList[0].point += 1;
+  console.log(itemList);
   return (
     <>
       <ItemBoxTemplate hide={isHide}>
@@ -34,6 +62,8 @@ const QuestionItem = () => {
           onClick={() => {
             setIsHide(true);
             setIsHide1(false);
+            itemList.itemList[1].point += 1;
+            console.log(itemList.itemList[1].point);
           }}
         >
           매운거
